d4c7f64009a04ae8de95b194662d9fede2d1dbdc
Chemputer <46601333+chemputer@users.noreply.github.com>
2021-04-05 15:52:43 -0400

    Initial commit, working nuspec and nupkg, comments removed from ps1 files in tools

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,2 @@
+﻿$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+Get-ChocolateyWebFile -PackageName 'sign4j' -FileFullPath "$toolsDir\sign4j.exe" -Url 'https://github.com/fbergmann/launch4j/raw/master/sign4j/sign4j.exe' -Checksum '1E54C719DDB8562AE2B35A7FEB8090393702F34C' -ChecksumType 'sha1'
