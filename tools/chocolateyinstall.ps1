3ae5a6ce84a51be39472fdefd9ab23ffb965da7e
Chemputer <46601333+chemputer@users.noreply.github.com>
2021-05-08 17:02:29 -0400

    made changes requested by chocolatey moderators

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,2 +1,9 @@
 ﻿$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-Get-ChocolateyWebFile -PackageName 'sign4j' -FileFullPath "$toolsDir\sign4j.exe" -Url 'https://github.com/fbergmann/launch4j/raw/master/sign4j/sign4j.exe' -Checksum '1E54C719DDB8562AE2B35A7FEB8090393702F34C' -ChecksumType 'sha1'
+$packageArgs = @{
+    packageName = 'sign4j'
+    fileFullPath = "$toolsDir\sign4j.exe"
+    url = 'https://github.com/fbergmann/launch4j/raw/master/sign4j/sign4j.exe'
+    Checksum = '1E54C719DDB8562AE2B35A7FEB8090393702F34C'
+    ChecksumType = 'sha1'
+}
+Get-ChocolateyWebFile $packageArgs 
